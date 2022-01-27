d4cc0efe29755cb4352788b98cff53ca6d7e30bb
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2022-01-27 20:10:46 +0200

    Add new feature : Notifications

---
+++ app/src/main/java/com/hamza/Wazaker/WorkManager/NotificationWorker.java
@@ -0,0 +1,32 @@
+package com.hamza.Wazaker.WorkManager;
+
+
+import android.content.Context;
+
+import androidx.annotation.NonNull;
+import androidx.work.Worker;
+import androidx.work.WorkerParameters;
+
+import com.hamza.Wazaker.Activites.MainActivity.Notifications;
+
+public class NotificationWorker extends Worker {
+
+    public NotificationWorker(@NonNull Context context, @NonNull WorkerParameters workerParams) {
+        super(context, workerParams);
+    }
+
+    @NonNull
+    @Override
+    public Result doWork() {
+        Notifications.addNotification(getApplicationContext());
+        return Result.success();
+    }
+}
+
+
+
+
+
+
+
+

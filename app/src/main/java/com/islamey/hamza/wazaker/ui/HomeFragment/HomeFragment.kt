fa637d80391435946b1087f7935cff0ad8e87f0b
Hamza Gamal <54688005+hamza94max@users.noreply.github.com>
2023-11-30 12:48:26 +0200

    update ui

--- app/src/main/java/com/islamey/hamza/wazaker/ui/HomeFragment/HomeFragment.kt
+++ app/src/main/java/com/islamey/hamza/wazaker/ui/HomeFragment/HomeFragment.kt
@@ -1,27 +1,20 @@
 package com.islamey.hamza.wazaker.ui.HomeFragment
 
 import android.annotation.SuppressLint
-import android.content.Intent
-import android.content.SharedPreferences
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import com.hassanjamil.hqibla.CompassActivity
-import com.hassanjamil.hqibla.Constants
+import com.islamey.hamza.wazaker.utils.Utils.getTotalCounts
 import com.islamey.wazkar.R
 import com.islamey.wazkar.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
-    private val binding get() = _binding!!
+    private lateinit var binding: FragmentHomeBinding
 
-    private val MY_PREFS_NAME = "pref"
-    private var sharedPreferences: SharedPreferences? = null
     private var totalcounts = 0
 
     override fun onStart() {
@@ -34,10 +27,7 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
 
-        _binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
-
-        sharedPreferences =
-            context?.getSharedPreferences(MY_PREFS_NAME, AppCompatActivity.MODE_PRIVATE)
+        binding = FragmentHomeBinding.inflate(layoutInflater, container, false)
 
         return binding.root
     }
@@ -60,50 +50,22 @@ class HomeFragment : Fragment() {
             findNavController().navigate(action)
         }
 
-        binding.fortyHadithbtn.setOnClickListener {
-            val action = HomeFragmentDirections.actionMainFragmentToFortyHadithListFragment()
-            findNavController().navigate(action)
-        }
-
         binding.counterBtn.setOnClickListener {
             val action = HomeFragmentDirections.actionMainFragmentToCounterFragment()
             findNavController().navigate(action)
         }
 
-        binding.qiblaBtn.setOnClickListener {
-            openQiblaActivity()
-        }
-
     }
 
     private fun getTotalZekerCountsFromSharedPreferences() {
-        totalcounts = sharedPreferences!!.getInt("zekertotalcounts", 0)
-        setTotalzekerCountstext()
+        totalcounts = getTotalCounts(requireContext())
+        setTotalZekerCountsText()
     }
 
     @SuppressLint("SetTextI18n")
-    private fun setTotalzekerCountstext() {
+    private fun setTotalZekerCountsText() {
         binding.totalZekercounts.text = getString(R.string.totalzeker) + "  " + totalcounts
     }
 
-    fun openQiblaActivity() {
-        val intent = Intent(context, CompassActivity::class.java)
-        intent.putExtra(Constants.TOOLBAR_BG_COLOR, "#FFFFFF") // Toolbar Background color
-        intent.putExtra(Constants.COMPASS_BG_COLOR, "#FFFFFF") // Compass background color
-        intent.putExtra(Constants.ANGLE_TEXT_COLOR, "#000000") // Angle Text color
-        intent.putExtra(Constants.DRAWABLE_DIAL, R.drawable.dial) // Your dial drawable resource
-        intent.putExtra(
-            Constants.DRAWABLE_QIBLA,
-            R.drawable.qibla
-        ) // Your qibla indicator drawable resource
-        intent.putExtra(
-            Constants.FOOTER_IMAGE_VISIBLE,
-            View.VISIBLE or View.INVISIBLE or View.GONE
-        ) // Footer World Image visibility
-        intent.putExtra(
-            Constants.LOCATION_TEXT_VISIBLE,
-            View.VISIBLE or View.INVISIBLE or View.GONE
-        ) // Location Text visibility
-        startActivity(intent)
-    }
+
 }
